ebb43fe40900e0148716fb47d04c8bdf182a8994
금가경 <gageum0@gmail.com>
2025-07-31 16:48:29 +0900

    Set environment tintColor and background

--- Moviepedia/SceneDelegate.swift
+++ Moviepedia/SceneDelegate.swift
@@ -16,6 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
+        window?.backgroundColor = .B
+        window?.tintColor = .Green
+        
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
