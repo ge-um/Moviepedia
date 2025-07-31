4bdaad9213cc8f2b46f427681408788831a4d4df
금가경 <gageum0@gmail.com>
2025-07-31 16:30:26 +0900

    Add colorset

---
+++ Moviepedia/Resource/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extension.swift
+//  Moviepedia
+//
+//  Created by 금가경 on 7/31/25.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+}
+
+extension UIColor {
+    static let Green = UIColor(hex: "98FB98")
+    static let Gray = UIColor(hex: "E1E1E1")
+    static let Gray2 = UIColor(hex: "8E8E8E")
+    static let W = UIColor(hex: "FFFFFF")
+    static let B = UIColor(hex: "000000")
+}
