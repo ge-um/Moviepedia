b3c4244f8d0976ffbde4d9d0f68051c7dccbbc30
금가경 <gageum0@gmail.com>
2025-08-08 01:15:07 +0900

    Delete NicknameDetailView

--- Moviepedia/Feature/Nickname/NicknameDetailViewController.swift
+++ Moviepedia/Feature/Nickname/NicknameDetailViewController.swift
@@ -8,28 +8,75 @@
 import UIKit
 
 // TODO: - 클로저 구문 없애기
-class NicknameDetailViewController: UIViewController {
-    private let detailNicknameView = NicknameDetailView()
+final class NicknameDetailViewController: UIViewController {
+    
+    let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .W
+        textField.font = .systemFont(ofSize: 13)
+        
+        return textField
+    }()
+    
+    private let line: UIView = {
+        let view = UIView()
+        view.backgroundColor = .W
+        
+        return view
+    }()
+    
+    let errorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = .Green
+        
+        return label
+    }()
     
     var sendNickname: ((User) -> Void)?
-        
-    override func loadView() {
-        view = detailNicknameView
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureSubview()
+        configureConstraint()
         configureNavigation()
         bindAction()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        let nickname = User(name: detailNicknameView.nicknameTextField.text!)
+        let nickname = User(name: nicknameTextField.text!)
         sendNickname?(nickname)
     }
 }
 
 extension NicknameDetailViewController: ViewControllerProtocol {
+    
+    func configureSubview() {
+        view.addSubview(nicknameTextField)
+        view.addSubview(line)
+        view.addSubview(errorLabel)
+    }
+    
+    func configureConstraint() {
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
+            make.height.equalTo(40)
+        }
+        
+        line.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom)
+            make.horizontalEdges.equalTo(nicknameTextField)
+            make.height.equalTo(1)
+        }
+        
+        errorLabel.snp.makeConstraints { make in
+            make.top.equalTo(line.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(nicknameTextField).inset(8)
+        }
+    }
+    
     func configureNavigation() {
         if let _ = navigationController?.viewControllers.first as? EditNicknameViewController {
             navigationItem.title = "닉네임 편집"
@@ -39,16 +86,16 @@ extension NicknameDetailViewController: ViewControllerProtocol {
     }
     
     func bindAction() {
-        detailNicknameView.nicknameTextField.becomeFirstResponder()
+        nicknameTextField.becomeFirstResponder()
         
-        detailNicknameView.nicknameTextField.addTarget(self, action: #selector(nicknameTextFieldEditingDidChange), for: .editingChanged)
+        nicknameTextField.addTarget(self, action: #selector(nicknameTextFieldEditingDidChange), for: .editingChanged)
     }
     
     // TODO: - 에러는 묶었는데 성공은 묶을 수 없을까?
     @objc func nicknameTextFieldEditingDidChange() {
-        let name = detailNicknameView.nicknameTextField.text!
+        let name = nicknameTextField.text!
         guard !name.isEmpty else {
-            detailNicknameView.errorLabel.text = ""
+            errorLabel.text = ""
             return
         }
         
@@ -62,6 +109,6 @@ extension NicknameDetailViewController: ViewControllerProtocol {
             message = error.localizedDescription
         }
         
-        detailNicknameView.errorLabel.text = message
+        errorLabel.text = message
     }
 }
