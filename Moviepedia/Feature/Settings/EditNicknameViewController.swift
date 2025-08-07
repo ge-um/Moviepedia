b8527f284381222f55829f12d0cd1fdfba70cea1
금가경 <gageum0@gmail.com>
2025-08-07 21:05:16 +0900

    Remove EditNicknameView

--- Moviepedia/Feature/Settings/EditNicknameViewController.swift
+++ Moviepedia/Feature/Settings/EditNicknameViewController.swift
@@ -11,22 +11,95 @@ final class EditNicknameViewController: UIViewController {
     
     private let editNicknameView = EditNicknameView()
     
-    private var nickname = User(name: "")
+    let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.text = AppSetting.nickname
+        textField.textColor = .W
+        textField.font = .systemFont(ofSize: 13)
+        textField.isEnabled = false
+        
+        return textField
+    }()
     
-    override func loadView() {
-        view = editNicknameView
-    }
+    let editButton: UIButton = {
+        let button = UIButton()
+        
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = .init(top: 8, leading: 20, bottom: 8, trailing: 20)
+        let container = AttributeContainer([.font: UIFont.systemFont(ofSize: 14, weight: .bold), .foregroundColor: UIColor.W])
+        
+        config.attributedTitle = AttributedString("편집", attributes: container)
+        
+        config.cornerStyle = .capsule
+        config.background.strokeColor = .W
+        config.background.strokeWidth = 1
+        
+        button.configuration = config
+        
+        return button
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 0
+        
+        return stackView
+    }()
+    
+    private let line: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .W
+        
+        return view
+    }()
+
+    private var nickname = User(name: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureNavigation()
+        
+        configureSubview()
+        configureConstraint()
+        configureStyle()
         bindAction()
     }
 }
 
 extension EditNicknameViewController: ViewControllerProtocol {
     
-    func configureNavigation() {
+    func configureSubview() {
+        stackView.addArrangedSubview(nicknameTextField)
+        stackView.addArrangedSubview(editButton)
+        
+        view.addSubview(stackView)
+        view.addSubview(line)
+    }
+    
+    func configureConstraint() {
+        nicknameTextField.snp.contentHuggingHorizontalPriority = 249
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
+            make.height.equalTo(40)
+        }
+        
+        line.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.bottom)
+            make.leading.equalTo(stackView)
+            make.trailing.equalTo(stackView).inset(42)
+            make.height.equalTo(1)
+        }
+    }
+    
+    func configureStyle() {
+        view.backgroundColor = .B
+        
         navigationItem.title = "닉네임 편집"
         
         let rightItem = UIBarButtonItem(title: "저장",
